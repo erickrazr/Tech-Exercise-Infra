17aae3e08cd8936b55c95c56042da3977afcd685
Erick Ferreira <erickrazr@gmail.com>
2024-04-09 15:46:57 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key"
+  type        = string
+  default     = "myaccessKey"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Access Key"
+  type        = string
+  default     = "mysecret"
+}
