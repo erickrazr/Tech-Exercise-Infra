f0b1f70c924742586d1c1dd8cdd62b87654e8b99
Erick Ferreira <erickrazr@gmail.com>
2024-04-09 23:40:38 -0300

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,19 +1,17 @@
 resource "aws_s3_bucket" "wte-mongo-backup" {
   bucket = "wte-mongo-backup" # Ensure this name is globally unique
-  acl    = "public-read"      # This makes the bucket publicly readable
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::wte-mongo-backup/*"
-    }
-  ]
 }
-POLICY
+
+resource "aws_s3_bucket_acl" "wte-mongo-backup_acl" {
+  bucket = aws_s3_bucket.wte-mongo-backup.bucket
+  acl    = "public"
+}
+
+resource "aws_s3_bucket_public_access_block" "wte-mongo-backup-public-allow" {
+  bucket = aws_s3_bucket.wte-mongo-backup.bucket
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
 }
