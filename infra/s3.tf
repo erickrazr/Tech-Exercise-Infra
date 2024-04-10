87b4ca698d2cfcea6664341326d6b2c324c992b5
Erick Ferreira <erickrazr@gmail.com>
2024-04-09 23:45:49 -0300

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -6,12 +6,3 @@ resource "aws_s3_bucket_acl" "wte-mongo-backup_acl" {
   bucket = aws_s3_bucket.wte-mongo-backup.bucket
   acl    = "public-read"
 }
-
-resource "aws_s3_bucket_public_access_block" "wte-mongo-backup-public-allow" {
-  bucket = aws_s3_bucket.wte-mongo-backup.bucket
-
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
-}
