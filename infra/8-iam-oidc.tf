dddbed10b5ff976d6b8f101a053138e0776bef39
Erick Ferreira <erickrazr@gmail.com>
2024-12-03 17:03:16 -0300

    Update 8-iam-oidc.tf

--- infra/8-iam-oidc.tf
+++ infra/8-iam-oidc.tf
@@ -3,7 +3,7 @@ data "tls_certificate" "eks" {
 }
 
 resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["https://sts.us-east-1.amazonaws.com"]
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
 }
