b1ea1bf1596d9bc5101dd2a9fa63c6728b7b2e56
Erick Ferreira <erickrazr@gmail.com>
2024-12-04 06:34:17 -0300

    Delete infra/s3.tf

--- infra/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "wte-mongo-backup" {
-  bucket = "wte-mongo-backup" # Ensure this name is globally unique
-}
-
-resource "aws_s3_bucket_acl" "wte-mongo-backup_acl" {
-  bucket = aws_s3_bucket.wte-mongo-backup.bucket
-  acl    = "public-read"
-}
